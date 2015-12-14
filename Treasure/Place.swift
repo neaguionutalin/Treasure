9f29585fdd24527806e890b8557f2f7ddf5b6068
Ionut Neagu <neagui@flg05-mac-08.coventry.ac.uk>
2015-12-14 12:30:37 +0000

    a

---
+++ Treasure/Place.swift
@@ -0,0 +1,13 @@
+//
+//  Place.swift
+//  Treasure
+//
+//  Created by Ionut Neagu on 14/12/2015.
+//  Copyright © 2015 Ionut Neagu. All rights reserved.
+//
+
+import UIKit
+
+class Place: NSObject {
+
+}
